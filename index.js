0c17b9bab2723c6c8f95fc31f869bf480f9b987b
adacameron <ada.e.cameron@gmail.com>
2021-09-07 15:53:08 +0100

    Setting up tests & database

---
+++ index.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const app = require('./src/app');
+
+const APP_PORT = 4000;
+
+app.listen(APP_PORT, () => {
+    console.log(`App is listening on port ${APP_PORT}`)
+})
+
---
+++ src/app.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// src/app.js
+const express = require('express');
+const artistRouter = require('./routes/artist');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/artist', artistRouter);
+
+
+app.get('/', (req, res) => {
+    res.status(200).json('hello world')
+    // res.send('world')
+    // res.status(200).send({ result: 'string'})
+});
+
+module.exports = app;
