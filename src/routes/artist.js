4527bdef66f0cb09371a45697e88f6bed3d4ae8f
adacameron <ada.e.cameron@gmail.com>
2021-09-09 14:54:06 +0100

    Routes for Artist delete & update

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -7,4 +7,6 @@ const router = express.Router();
 router.post('/', artistController.create);
 router.get('/', artistController.read);
 router.get('/:artistId', artistController.readById);
+router.patch('/:artistId', artistController.update);
+router.delete('/:artistId', artistController.delete)
 module.exports = router;
